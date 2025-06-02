fc5a17b29a017e6ee03c259d916e3f5ff5997c9a
Dungewar <dungewar@gmail.com>
2025-06-02 15:21:58 -0700

    Do nothing

--- data.lua
+++ data.lua
@@ -1,53 +1,53 @@
 --data.lua
 
-local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) -- copy the table that defines the heavy armor item into the fireArmor variable
+-- local fireArmor = table.deepcopy(data.raw["armor"]["heavy-armor"]) -- copy the table that defines the heavy armor item into the fireArmor variable
 
-fireArmor.name = "fire-armor"
-fireArmor.icons = {
-  {
-    icon = fireArmor.icon,
-    icon_size = fireArmor.icon_size,
-    tint = { r = 1, g = 0, b = 0, a = 0.3 }
-  },
-}
+-- fireArmor.name = "fire-armor"
+-- fireArmor.icons = {
+--   {
+--     icon = fireArmor.icon,
+--     icon_size = fireArmor.icon_size,
+--     tint = { r = 1, g = 0, b = 0, a = 0.3 }
+--   },
+-- }
 
-fireArmor.resistances = {
-  {
-    type = "physical",
-    decrease = 6,
-    percent = 10
-  },
-  {
-    type = "explosion",
-    decrease = 10,
-    percent = 30
-  },
-  {
-    type = "acid",
-    decrease = 5,
-    percent = 30
-  },
-  {
-    type = "fire",
-    decrease = 0,
-    percent = 100
-  }
-}
+-- fireArmor.resistances = {
+--   {
+--     type = "physical",
+--     decrease = 6,
+--     percent = 10
+--   },
+--   {
+--     type = "explosion",
+--     decrease = 10,
+--     percent = 30
+--   },
+--   {
+--     type = "acid",
+--     decrease = 5,
+--     percent = 30
+--   },
+--   {
+--     type = "fire",
+--     decrease = 0,
+--     percent = 100
+--   }
+-- }
 
--- create the recipe prototype from scratch
-local fireArmorRecipe = {
-  type = "recipe",
-  name = "fire-armor",
-  enabled = true,
-  energy_required = 8, -- time to craft in seconds (at crafting speed 1)
-  ingredients = {
-    { type = "item", name = "copper-plate", amount = 200 },
-    { type = "item", name = "steel-plate",  amount = 50 }
-  },
-  results = { { type = "item", name = "fire-armor", amount = 1 } }
-}
+-- -- create the recipe prototype from scratch
+-- local fireArmorRecipe = {
+--   type = "recipe",
+--   name = "fire-armor",
+--   enabled = true,
+--   energy_required = 8, -- time to craft in seconds (at crafting speed 1)
+--   ingredients = {
+--     { type = "item", name = "copper-plate", amount = 200 },
+--     { type = "item", name = "steel-plate",  amount = 50 }
+--   },
+--   results = { { type = "item", name = "fire-armor", amount = 1 } }
+-- }
 
-data:extend { fireArmor, fireArmorRecipe }
+-- data:extend { fireArmor, fireArmorRecipe }
 
 
 local beryllium = table.deepcopy(data.raw["item"]["iron-plate"]) -- copy the table that defines the heavy armor item into the fireArmor variable
@@ -75,7 +75,7 @@ local berylliumRecipe = {
   type = "recipe",
   name = "beryllium",
   category = "crafting-with-fluid",
-  icon = beryllium.icon,
+  main_product = "beryllium",
   enabled = true,
   energy_required = 10, -- time to craft in seconds (at crafting speed 1)
   ingredients = {
